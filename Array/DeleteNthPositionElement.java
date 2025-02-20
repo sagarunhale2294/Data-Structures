cad97dbc2fafd703cdef36396413339d3811dd91
Sagarkumar Unhale <sagarunhale2294@gmail.com>
2025-02-20 22:16:27 +0530

    Create DeleteNthPositionElement.java

---
+++ Array/DeleteNthPositionElement.java
@@ -0,0 +1,18 @@
+class DeleteNthPositionElement {
+  
+    public static void deleteElement(int arr[], int position) {
+      for(int i = position; i < arr.length-1; i++) {
+        arr[i] = arr[i+1];
+      }
+      for (int i=0; i<arr.length-1; i++) {
+        System.out.println(arr[i]);
+      }
+    }
+  
+    public static void main(String[] args) {
+      int array[] = {1,2,3,4,5};
+      int position = 2;
+      
+      DeleteNthPositionElement.deleteElement(array, position);
+    }
+}
